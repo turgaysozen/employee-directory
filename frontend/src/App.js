66391805e34ef7ecdf249bf294e8c3f6c509d074
Turgay <turgaysozen@hotmail.com>
2022-11-26 20:27:45 +0300

    Update main app and css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css';
+import Users from './Users/Users';
+import { getUsers } from '../src/Users/getUsers';
+import UserDetail from './Users/UserDetail';
+import { useParams } from "react-router-dom"
 
 function App() {
+  const [userCount, setUserCount] = useState()
+  const [totalUser, setTotalUser] = useState()
+  const [users, setUsers] = useState()
+
+  // get users count per page, total count and pass them to User component
+  useEffect(() => {
+    (async () => {
+      const users = await getUsers()
+      setUserCount(users.per_page)
+      setTotalUser(users.total)
+      setUsers(users.data)
+    })()
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Users userCount={userCount} totalUserCount={totalUser} />}></Route>
+            <Route path='users/:id' element={<UserDetail />}>
+            </Route>
+          </Routes>
+        </BrowserRouter>
       </header>
     </div>
   );
