135ab6ce510607010cccdaf4810b012513d9399d
Turgay <turgaysozen@hotmail.com>
2022-11-27 18:51:47 +0300

    Change React env variables

--- client/src/components/Users/getUsers.js
+++ client/src/components/Users/getUsers.js
@@ -2,10 +2,8 @@ import axios from 'axios';
 
 // fetch users from backend service
 export let getUsers = async (page) => {
-    const address = process.env.REACT_APP_ROOT_URL
-    const endpoint = process.env.REACT_APP_END_POINT
-    const port = process.env.REACT_APP_PORT
-    const url = `${address}:${port}${endpoint}/users?page=${page}`
+    const apiUrl = process.env.REACT_APP_API_URL
+    const url = `${apiUrl}users?page=${page}`
     const res = await axios(url)
     return res.data
 }
