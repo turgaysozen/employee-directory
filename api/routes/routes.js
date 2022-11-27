44fe45cb2261a9cdaf0850264ab27930f7486b53
Turgay <turgaysozen@hotmail.com>
2022-11-28 01:10:54 +0300

    Clean routes file

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -4,12 +4,6 @@ const router = express.Router()
 const getUser = require('../utils/external_calls')
 const cache = require('../cache')
 
-// router.get('/users', async (req, res) => {
-//     const { page } = req.query
-//     const users = await getUser(page)
-//     res.send(users)
-// })
-
 router.get('/users', cache, getUser)
 
 module.exports = router;
